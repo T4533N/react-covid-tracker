113dece3156a0f80ed6303073a897c3dc79fb095
taseentanvir <taseentanvir@gmail.com>
2021-04-01 22:09:35 +0600

    final product

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import { Stack } from '@hadermania/components';
+
+export default function About() {
+  return (
+    <Stack as="section">
+      <h1>.</h1>
+      <h1>.</h1>
+    </Stack>
+  );
+}
